4dc0d6f4629d08c5a37f94de44d5ffd79f600651
rouge9 <99325915+rouge9@users.noreply.github.com>
2022-03-14 07:03:28 -0800

    Add files via upload

--- working_with_csv.py
+++ working_with_csv.py
@@ -8,8 +8,8 @@
 #
 #     print(temperature)
 
-import pandas
-data = pandas.read_csv("weather-data.csv")
+# import pandas
+# data = pandas.read_csv("weather-data.csv")
 # data_dict = data.to_dict()
 # temp_list = data["temp"].to_list()
 # # get data in column
@@ -19,4 +19,21 @@ data = pandas.read_csv("weather-data.csv")
 
 # get data in row
 
-print(data[data.temp == data.temp.max()])
+# print(data[data.temp == data.temp.max()])
+
+import pandas
+data = pandas.read_csv("227 2018-Central-Park-Squirrel-Census-Squirrel-Data.csv")
+grey = len(data[data["Primary Fur Color"] == "Gray"])
+print(grey)
+black = len(data[data["Primary Fur Color"] == "Black"])
+print(black)
+cinnamon = len(data[data["Primary Fur Color"] == "Cinnamon"])
+print(cinnamon)
+
+data_dict = {
+    "fur color": ["Grey", "black", "cinnamon"],
+    "count": [grey, black, cinnamon]
+}
+
+df = pandas.DataFrame(data_dict)
+df.to_csv("fur color.csv")
