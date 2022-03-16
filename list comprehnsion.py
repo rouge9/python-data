9c4515fed3b1d071d88cc6e86d68c1df8606d3b4
rouge9 <99325915+rouge9@users.noreply.github.com>
2022-03-15 22:32:46 -0800

    simple exercise

---
+++ list comprehnsion.py
@@ -0,0 +1,10 @@
+# Squired number
+numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+squired_number = [num ** 2 for num in numbers]
+print(squired_number)
+
+# even numbers
+even = [num for num in numbers if num % 2 == 0]
+print(even)
+
+
