7e56336e8b6e070102870d4009b86cce9c257858
rouge9 <99325915+rouge9@users.noreply.github.com>
2022-03-16 08:28:41 -0800

    done third question

--- list comprehnsion.py
+++ list comprehnsion.py
@@ -7,4 +7,13 @@ print(squired_number)
 even = [num for num in numbers if num % 2 == 0]
 print(even)
 
+# checking if number in both files
 
+with open("file_1.txt") as file1:
+    fiel_1_data = file1.readlines()
+
+with open("file_2.txt") as file2:
+    fiel_2_data = file2.readlines()
+
+result = [int(num) for num in fiel_1_data if num in fiel_2_data]
+print(result)
