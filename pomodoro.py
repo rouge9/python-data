830002cc8b81ba6847f5ef819592b1eead0c22f9
rouge9 <99325915+rouge9@users.noreply.github.com>
2022-03-24 01:50:19 -0800

    pomodoro timer

--- pomodoro.py
+++ pomodoro.py
@@ -7,29 +7,58 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
-SHORT_BREAK_MIN = 5
+WORK_MIN = 1
+SHORT_BREAK_MIN = 1
 LONG_BREAK_MIN = 20
+reps = 0
+timer = None
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
-
+def reset():
+    global reps
+    canvas.after_cancel(timer)
+    canvas.itemconfig(timer_canvas, text="00:00")
+    timer_label.config(text="Timer")
+    checkmark_label.config(text="")
+    reps = 0
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
-    count_down(WORK_MIN * 60)
+    global reps
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_sec = SHORT_BREAK_MIN * 60
+    long_sec = LONG_BREAK_MIN * 60
+    if reps % 8 == 0:
+        count_down(long_sec)
+        timer_label.config(text="Break", fg=RED)
+    elif reps % 2 == 0:
+        count_down(short_sec)
+        timer_label.config(text="Break", fg=PINK)
+    else:
+        count_down(work_sec)
+        timer_label.config(text="Work", fg=GREEN)
+
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
     count_min = math.floor(count / 60)
     count_sec = count % 60
-    if count_sec == 0 & count_sec < 10:
+    if count_sec < 10:
         count_sec = f"0{count_sec}"
 
     canvas.itemconfig(timer_canvas, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, count_down, count - 1)
-
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        work_session = math.floor(reps / 2)
+        marks = ""
+        for _ in range(work_session):
+            marks += "✔"
+        checkmark_label.config(text=marks)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -47,9 +76,9 @@ timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 40, "bol
 timer_label.grid(column=1, row=0)
 start_button = Button(text="Start", command=start_timer)
 start_button.grid(column=0, row=2)
-reset_button = Button(text="Reset")
+reset_button = Button(text="Reset", command=reset)
 reset_button.grid(column=2, row=2)
-checkmark_label = Label(text="✔", fg=GREEN, bg=YELLOW, )
+checkmark_label = Label(fg=GREEN, bg=YELLOW, )
 checkmark_label.grid(column=1, row=3)
 
 window.mainloop()
